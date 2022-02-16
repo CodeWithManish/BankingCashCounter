d3f14c3503b361e45da74b4753c2dfc95962d9dd
Manish <manishkumar0156@gmail.com>
2022-02-16 20:39:24 +0530

    [Manish] Add: Banking cash counter operation

---
+++ BankingCashCounter/CashCounter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingCashCounter
+{
+    public class CashCounter
+    {
+        int deposit = 0;
+        int withdraw =0;
+        double totalBalance = 1000;
+
+        QueueClass queue=new QueueClass();
+        public void Deposit()
+        {
+            Console.WriteLine("Total Amount of your Account:" + totalBalance);
+            Console.WriteLine("Enter Deposit Amount: ");
+            deposit = Convert.ToInt32(Console.ReadLine());
+            totalBalance = totalBalance + deposit;
+            System.Console.WriteLine(deposit + " deposit money in your account\n");
+            System.Console.WriteLine("Current Avaialble Balance is :" + totalBalance);
+            
+            queue.Dequeue();
+        }
+        
+        public void Withdraw()
+        {
+            Console.WriteLine("Total Amount of your Account:" + totalBalance);
+            Console.WriteLine("Enter Withdraw Amount: ");
+            withdraw =Convert.ToInt32(Console.ReadLine());
+            if (withdraw > totalBalance)
+            {
+                System.Console.WriteLine("Withdraw Amount does not Exist your Account\n");
+            }
+            else
+            {
+                totalBalance = totalBalance - withdraw;
+                System.Console.WriteLine(withdraw + "Amount withdraw from your Account\n");
+                System.Console.WriteLine("Current Avaialble Balance is :"+ totalBalance);
+            }
+            queue.Dequeue();
+        }
+       
+    }
+}
---
+++ BankingCashCounter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingCashCounter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Welcome to Banking Cash Counter");
+            
+            CashCounter counter = new CashCounter();
+            QueueClass queue = new QueueClass();
+
+            Random random = new Random();
+            int user = random.Next(1, 11);
+            System.Console.WriteLine(user + " Users are in Cash Counter queue ");
+
+            System.Console.WriteLine("Press 1. Deposit\nPress 2.Withdraw\n ");
+                                               
+            for (int i = 1; i <= user; i++)
+            {
+                int check = Convert.ToInt32(Console.ReadLine());
+
+                switch (check)
+                {
+                    case 1:
+                        queue.Enqueue(i);
+                        counter.Deposit();
+                        break;
+
+                    case 2:
+                        queue.Enqueue(i);
+                        counter.Withdraw();
+                        break;
+
+                    default:
+                        System.Console.WriteLine("Invalid Selection!");
+                        break;
+                }
+            }
+        }
+    }
+}
